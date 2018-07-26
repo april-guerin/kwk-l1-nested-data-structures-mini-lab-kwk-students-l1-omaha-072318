399715e969c3d979b642cd2b3e6db23a4cf6d18d
april-guerin <aprilguerin@yahoo.com>
2018-07-26 21:09:31 +0000

    automatically backed up by learn

--- index.rb
+++ index.rb
@@ -7,7 +7,14 @@ ingredients = [
 ]
 
 def sandwich_request(ingredients, order) 
-  ingredients.include? 'order'
+  ingredients = [
+    ["rye", "sourdough", "baguette"],
+    ["ham", "salami", "turkey"],
+    ["swiss", "munster", "cheddar"],
+    ["mayo", "mustard", "tabasco"]
+]
+  order = ["rye", "tofu","cheddar"]
+  ingredients.include? order
 end
 
 # example city_info hash
